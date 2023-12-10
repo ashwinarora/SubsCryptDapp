75cb5a476fbe8643fd5b8155776e09d1cd3b4d37
Ashwin Arora <ashwinarora48@gmail.com>
2023-12-10 09:56:37 +0530

    Added arbitrum sepolia config

---
+++ src/ChainConfig/ArbitrumSepolia.ts
@@ -0,0 +1,9 @@
+import IChainConfig from "./IChainConfig";
+
+const BscTestnetConfig: IChainConfig = {
+    name: "Arbitrum Sepolia",
+    chainId: 421614,
+    subscryptAddress: "0x00297d2cE47927aC65c40cC3dc466958f6898797",
+}
+
+export default BscTestnetConfig;
