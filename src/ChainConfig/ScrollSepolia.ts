d24ca2a23ab0eeb376cbdd239761faa5597ac856
Ashwin Arora <ashwinarora48@gmail.com>
2023-12-10 10:05:33 +0530

    scroll sepolia config added

---
+++ src/ChainConfig/ScrollSepolia.ts
@@ -0,0 +1,9 @@
+import IChainConfig from "./IChainConfig";
+
+const BscTestnetConfig: IChainConfig = {
+    name: "Scroll Sepolia",
+    chainId: 534351 ,
+    subscryptAddress: "0x00297d2cE47927aC65c40cC3dc466958f6898797",
+}
+
+export default BscTestnetConfig;
--- src/ChainConfig/index.ts
+++ src/ChainConfig/index.ts
@@ -1,5 +1,6 @@
 import IChainConfig from "./IChainConfig";
 import BscTestnetConfig from "./BscTestnetConfig";
+import ArbitrumSepolia from "./ArbitrumSepolia";
 
 interface IChainConfigMap {
     [chainId: number]: IChainConfig;
@@ -7,6 +8,7 @@ interface IChainConfigMap {
 
 const allChains: IChainConfigMap = {
     [BscTestnetConfig.chainId]: BscTestnetConfig,
+    [ArbitrumSepolia.chainId]: ArbitrumSepolia,
 }
 
 export default allChains;
